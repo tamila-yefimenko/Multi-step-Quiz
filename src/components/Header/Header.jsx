08e57bc1b4cc96aa4050888ba9f3838e7bb337bf
Tamila Yefimenko <tamilaedu88@gmail.com>
2025-10-06 22:00:31 +0300

    fixed Header styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,13 +4,15 @@ import { Outlet, NavLink, useLocation } from "react-router-dom";
 const Header = () => {
   const location = useLocation();
   const addActive = ({ isActive }) =>
-    isActive ? "text-red-600 underline" : "text-white hover:text-blue-100";
+    isActive
+      ? "text-red-600 underline"
+      : "text-white hover:text-blue-100 focus:outline-none";
 
   return (
     <>
-      <header className="mx-auto flex max-w-100 justify-center rounded-xl p-6 shadow-lg outline-black/5 bg-orange-400/70">
+      <header className="mx-auto flex max-w-100 justify-center  p-6 shadow-lg outline-black/5 bg-orange-400/70">
         <nav>
-          <ul className="flex gap-x-6 text-xl font-medium ">
+          <ul className="flex gap-x-6 text-xl font-medium">
             <li>
               <NavLink to="/" className={addActive}>
                 Home
